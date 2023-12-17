82ef2d3830955592a641e19cc98db028e948dd65
Nathalie Casati <nat@yuka.ch>
2023-12-17 01:55:01 +0100

    refactoring for all node types

--- start_node.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -d 'data' ]; then
-    mkdir data
-fi
-
-UID_GID="$(id -u):$(id -g)" docker compose up -d
--- stop_node.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-UID_GID="$(id -u):$(id -g)" docker compose stop
