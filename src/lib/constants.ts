de8684bf2c46f2c0b454b3e1e51c441d8d462774
Pouya <pouya@kenshi.io>
2023-12-21 22:05:10 +0100

    Bump version

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,7 +7,7 @@ import {
   StringGossipMethodObject,
 } from "./types.js";
 
-export const version = "0.8.4";
+export const version = "0.8.5";
 export const protocolVersion = "0.8.0";
 
 export const topic = sha(`Kenshi.Unchained.Testnet.Topic.V${protocolVersion}`);
