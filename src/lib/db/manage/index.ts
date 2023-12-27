4d60c90350f4ac70d4bc520d82f4da766b872bac
Pouya <pouya@kenshi.io>
2023-12-27 15:48:58 +0100

    Add migration logic

---
+++ src/lib/db/manage/index.ts
@@ -0,0 +1,25 @@
+import { spawn } from "child_process";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { config } from "../../constants.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __root = new Array(5)
+  .fill(null)
+  .reduce((path) => dirname(path), __filename);
+
+export const runPrismaCommand = async (
+  args: readonly string[]
+): Promise<number | null> => {
+  const prisma = `prisma${process.platform === "win32" ? ".cmd" : ""}`;
+  const prismaCliPath = join(__root, "node_modules", ".bin", prisma);
+
+  const prismaProcess = spawn(prismaCliPath, args, {
+    stdio: "inherit",
+    env: { ...process.env, UNCHAINED_DATABASE_URL: config.database?.url },
+    shell: true,
+    cwd: __root,
+  });
+
+  return new Promise((resolve) => prismaProcess.on("close", resolve));
+};
