f9450b9dc3a5383472cec742c0a72d36a3a19f72
Pouya <pouya@kenshi.io>
2023-12-27 19:46:45 +0100

    Add prepare

--- dist/lib/bls/worker.js
+++
@@ -1,6 +0,0 @@
-import { parentPort } from "node:worker_threads";
-import { verify } from "./index.js";
-parentPort?.on("message", (task) => {
-    const result = verify(task);
-    parentPort?.postMessage(result);
-});
--- dist/lib/utils/hash.js
+++
@@ -1,4 +0,0 @@
-import crypto from "crypto";
-import serialize from "json-canon";
-export const sha = (input) => crypto.createHash("sha256").update(input).digest();
-export const hashObject = (object) => sha(serialize(object)).toString("hex");
