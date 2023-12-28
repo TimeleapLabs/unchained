963e6668bdb43fc9c923ffb3c8c7863283a9521f
Pouya <pouya@kenshi.io>
2023-12-28 17:19:46 +0100

    Rework docker entry point

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-if [ "$UNCHAINED_NODE_TYPE" == "full" ]; then
+echo "Running a $UNCHAINED_NODE_TYPE node."
+
+if [ "$UNCHAINED_NODE_TYPE" = "full" ]; then
   unchained postgres migrate conf.yaml
+  retVal=$?
+  if [ $retVal -ne 0 ]; then
+    exit $retVal
+  fi
 fi
 
 unchained start conf.yaml --generate
