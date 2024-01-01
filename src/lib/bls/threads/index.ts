06380a2b2149a2de7103c9c1becb6d598fbaf7b0
Pouya <pouya@kenshi.io>
2024-01-01 02:46:10 +0100

    Make the code a bit nicer

--- src/lib/bls/threads/index.ts
+++ src/lib/bls/threads/index.ts
@@ -77,8 +77,11 @@ export default class WorkerPool extends EventEmitter {
     worker.on("error", (err) => {
       // In case of an uncaught exception: Call the callback that was passed to
       // `runTask` with the error.
-      if (worker[kTaskInfo]) worker[kTaskInfo].done(err, null);
-      else this.emit("error", err);
+      if (worker[kTaskInfo]) {
+        worker[kTaskInfo].done(err, null);
+      } else {
+        this.emit("error", err);
+      }
       // Remove the worker from the list and start a new Worker to replace the
       // current one.
       this.workers.splice(this.workers.indexOf(worker), 1);
