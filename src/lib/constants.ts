1e3869653aabd56163b8298655f65a706470e4c4
Pouya <pouya@kenshi.io>
2024-01-04 21:29:32 +0100

    Minor fixes

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -8,7 +8,7 @@ import {
   MetaData,
 } from "./types.js";
 
-export const version = "0.9.2";
+export const version = "0.9.3";
 export const protocolVersion = "0.9.0";
 
 export const topic = sha(`Kenshi.Unchained.Testnet.Topic.V${protocolVersion}`);
