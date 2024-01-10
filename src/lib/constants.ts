e379f668a83a62d9692764aec22306e9c7db02bc
Pouya <pouya@kenshi.io>
2024-01-10 01:20:00 +0100

    Bump version

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -9,7 +9,7 @@ import {
   Murmur,
 } from "./types.js";
 
-export const version = "0.10.0";
+export const version = "0.10.1";
 export const protocolVersion = "0.10.0";
 
 export const topic = sha(`Kenshi.Unchained.Testnet.Topic.V${protocolVersion}`);
