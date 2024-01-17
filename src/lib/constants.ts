7fc8da67e056d86a0fbc4d75561e7f572d4702b6
Pouya <pouya@kenshi.io>
2024-01-17 23:09:47 +0100

    Hotfix for empty signer/signature

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -9,7 +9,7 @@ import {
   Murmur,
 } from "./types.js";
 
-export const version = "0.10.13";
+export const version = "0.10.14";
 export const protocolVersion = "0.10.13";
 
 export const topic = sha(`Kenshi.Unchained.Testnet.Topic.V${protocolVersion}`);
