36661bf91082c46d8a24ca8a008595c2daedda66
Nathalie Casati <nat+github@yuka.ch>
2024-01-31 01:25:12 +0100

    changed alpine to ubuntu

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker buildx build --build-arg="ALPINE_VERSION=latest" -t ghcr.io/kenshitech/unchained:latest --no-cache .
+docker buildx build --build-arg="UBUNTU_VERSION=latest" -t ghcr.io/kenshitech/unchained:latest --no-cache .
