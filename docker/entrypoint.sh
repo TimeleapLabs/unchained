8ec3f4bc2e54d864463df8f0de30283a86ab8b82
Nathalie Casati <nat+github@yuka.ch>
2024-02-01 01:16:48 +0100

    updated docker deployment for go

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,4 +10,4 @@ echo "Running a $UNCHAINED_NODE_TYPE node."
 #  fi
 #fi
 
-./unchained $UNCHAINED_NODE_TYPE -c conf.yaml
+./unchained $UNCHAINED_NODE_TYPE -c conf/conf.$UNCHAINED_NODE_TYPE.yaml -s conf/secrets.$UNCHAINED_NODE_TYPE.yaml
