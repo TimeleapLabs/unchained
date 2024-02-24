ee6caed89d2ab5824a934815264a3557741a27b4
Nathalie Casati <nat+github@yuka.ch>
2024-02-24 21:02:51 +0100

    added standalone node type

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -10,4 +10,4 @@ echo "Running a $UNCHAINED_NODE_TYPE node."
 #  fi
 #fi
 
-./unchained $UNCHAINED_NODE_TYPE -c conf/conf.$UNCHAINED_NODE_TYPE.yaml -s conf/secrets.$UNCHAINED_NODE_TYPE.yaml -x context
+./unchained $UNCHAINED_NODE_TYPE -c conf/conf.$UNCHAINED_NODE_TYPE.yaml -s conf/secrets.$UNCHAINED_NODE_TYPE.yaml -x context/$UNCHAINED_NODE_TYPE
