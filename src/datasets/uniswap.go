b791c0df088c4e017330f2103ce299172f81a563
Parya Rastegar <rastegar.parya3@gmail.com>
2024-03-26 02:06:23 +0330

    feat: add proto files for datasets

--- src/datasets/uniswap.go
+++ src/datasets/uniswap.go
@@ -1,37 +1,49 @@
 package datasets
 
 import (
-	"math/big"
-
-	"github.com/KenshiTech/unchained/crypto/bls"
+	"github.com/KenshiTech/unchained/constants"
+	"github.com/rs/zerolog/log"
+	"google.golang.org/protobuf/proto"
 )
 
-type TokenKey struct {
-	Name   string
-	Pair   string
-	Chain  string
-	Delta  int64
-	Invert bool
-	Cross  string
+//
+//type TokenKey struct {
+//	Name   string
+//	Pair   string
+//	Chain  string
+//	Delta  int64
+//	Invert bool
+//	Cross  string
+//}
+//
+//type AssetKey struct {
+//	Token TokenKey
+//	Block uint64
+//}
+//
+//type PriceInfo struct {
+//	Asset AssetKey
+//	Price big.Int
+//}
+//
+//type PriceReport struct {
+//	PriceInfo PriceInfo
+//	Signature [48]byte
+//}
+
+func (m *PriceReport) Protobuf() ([]byte, error) {
+	protoModel, err := proto.Marshal(m)
+	if err != nil {
+		log.Err(err)
+		return nil, constants.InternalError
+	}
+
+	return protoModel, nil
 }
 
-type AssetKey struct {
-	Token TokenKey
-	Block uint64
-}
-
-type PriceInfo struct {
-	Asset AssetKey
-	Price big.Int
-}
-
-type PriceReport struct {
-	PriceInfo PriceInfo
-	Signature [48]byte
-}
-
-type BroadcastPricePacket struct {
-	Info      PriceInfo
-	Signature [48]byte
-	Signer    bls.Signer
-}
+//
+//type BroadcastPricePacket struct {
+//	Info      PriceInfo
+//	Signature [48]byte
+//	Signer    bls.Signer
+//}
