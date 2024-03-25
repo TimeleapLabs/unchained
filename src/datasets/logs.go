e5f1da73e993c68ce859b11e3a314b4a2cd794b7
Parya Rastegar <rastegar.parya3@gmail.com>
2024-03-26 02:06:23 +0330

    feat: add proto files for datasets

--- src/datasets/logs.go
+++ src/datasets/logs.go
@@ -1,56 +1,49 @@
 package datasets
 
-import (
-	"encoding/json"
-
-	"github.com/KenshiTech/unchained/crypto/bls"
-	"github.com/vmihailenco/msgpack/v5"
-)
-
-type EventLogArg struct {
-	Name  string
-	Type  string
-	Value any
-}
-
-var _ msgpack.CustomEncoder = (*EventLogArg)(nil)
-
-// TODO: this can be improved
-func (eventLog *EventLogArg) EncodeMsgpack(enc *msgpack.Encoder) error {
-	encoded, err := json.Marshal(eventLog)
-	if err != nil {
-		return err
-	}
-	return enc.EncodeBytes(encoded)
-}
-
-var _ msgpack.CustomDecoder = (*EventLogArg)(nil)
-
-func (eventLog *EventLogArg) DecodeMsgpack(dec *msgpack.Decoder) error {
-	bytes, err := dec.DecodeBytes()
-	if err != nil {
-		return err
-	}
-	return json.Unmarshal(bytes, eventLog)
-}
-
-type EventLog struct {
-	LogIndex uint64
-	Block    uint64
-	Address  string
-	Event    string
-	Chain    string
-	TxHash   [32]byte
-	Args     []EventLogArg
-}
-
-type EventLogReport struct {
-	EventLog
-	Signature [48]byte
-}
-
-type BroadcastEventPacket struct {
-	Info      EventLog
-	Signature [48]byte
-	Signer    bls.Signer
-}
+//type EventLogArg struct {
+//	Name  string
+//	Type  string
+//	Value any
+//}
+//
+//var _ msgpack.CustomEncoder = (*EventLogArg)(nil)
+//
+//// TODO: this can be improved
+//func (eventLog *EventLogArg) EncodeMsgpack(enc *msgpack.Encoder) error {
+//	encoded, err := json.Marshal(eventLog)
+//	if err != nil {
+//		return err
+//	}
+//	return enc.EncodeBytes(encoded)
+//}
+//
+//var _ msgpack.CustomDecoder = (*EventLogArg)(nil)
+//
+//func (eventLog *EventLogArg) DecodeMsgpack(dec *msgpack.Decoder) error {
+//	bytes, err := dec.DecodeBytes()
+//	if err != nil {
+//		return err
+//	}
+//	return json.Unmarshal(bytes, eventLog)
+//}
+//
+//type EventLog struct {
+//	LogIndex uint64
+//	Block    uint64
+//	Address  string
+//	Event    string
+//	Chain    string
+//	TxHash   [32]byte
+//	Args     []EventLogArg
+//}
+//
+//type EventLogReport struct {
+//	EventLog
+//	Signature [48]byte
+//}
+//
+//type BroadcastEventPacket struct {
+//	Info      EventLog
+//	Signature [48]byte
+//	Signer    bls.Signer
+//}
