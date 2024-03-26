648fc909ad04de495a4717b1f7175f254e7c24eb
Parya Rastegar <rastegar.parya3@gmail.com>
2024-03-27 02:38:12 +0330

    fix: delete some comments

--- src/datasets/logs.go
+++ src/datasets/logs.go
@@ -6,33 +6,6 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-//type EventLogArg struct {
-//	Name  string
-//	Type  string
-//	Value any
-//}
-//
-//var _ msgpack.CustomEncoder = (*EventLogArg)(nil)
-//
-//// TODO: this can be improved
-//func (eventLog *EventLogArg) EncodeMsgpack(enc *msgpack.Encoder) error {
-//	encoded, err := json.Marshal(eventLog)
-//	if err != nil {
-//		return err
-//	}
-//	return enc.EncodeBytes(encoded)
-//}
-//
-//var _ msgpack.CustomDecoder = (*EventLogArg)(nil)
-//
-//func (eventLog *EventLogArg) DecodeMsgpack(dec *msgpack.Decoder) error {
-//	bytes, err := dec.DecodeBytes()
-//	if err != nil {
-//		return err
-//	}
-//	return json.Unmarshal(bytes, eventLog)
-//}
-
 func (m *EventLog) Protobuf() ([]byte, error) {
 	protoModel, err := proto.Marshal(m)
 	if err != nil {
@@ -72,24 +45,3 @@ func (m *BroadcastEventPacket) Protobuf() ([]byte, error) {
 
 	return protoModel, nil
 }
-
-//type EventLog struct {
-//	LogIndex uint64
-//	Block    uint64
-//	Address  string
-//	Event    string
-//	Chain    string
-//	TxHash   [32]byte
-//	Args     []EventLogArg
-//}
-//
-//type EventLogReport struct {
-//	EventLog
-//	Signature [48]byte
-//}
-//
-//type BroadcastEventPacket struct {
-//	Info      EventLog
-//	Signature [48]byte
-//	Signer    bls.Signer
-//}
--- src/datasets/uniswap.go
+++ src/datasets/uniswap.go
@@ -7,21 +7,6 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
-//
-//type TokenKey struct {
-//	Name   string
-//	Pair   string
-//	Chain  string
-//	Delta  int64
-//	Invert bool
-//	Cross  string
-//}
-//
-//type AssetKey struct {
-//	Token TokenKey
-//	Block uint64
-//}
-
 func (m *TokenKey) Protobuf() ([]byte, error) {
 	protoModel, err := proto.Marshal(m)
 	if err != nil {
