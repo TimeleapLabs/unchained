67c95d7c15ee2d324c43f1c35da49d07d3357cc2
Parya Rastegar <rastegar.parya3@gmail.com>
2024-04-05 02:38:29 +0330

    fix: remove go.work

--- internal/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/KenshiTech/unchained/cmd"
-
-func main() {
-	cmd.Execute()
-}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/KenshiTech/unchained/internal/cmd"
+
+func main() {
+	cmd.Execute()
+}
