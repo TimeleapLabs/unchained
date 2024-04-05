bb4a7cd550f458086af93c619bc380e737772e4b
Parya Rastegar <rastegar.parya3@gmail.com>
2024-04-05 03:38:55 +0330

    fix: linter

--- internal/config/model.go
+++ internal/config/model.go
@@ -61,7 +61,7 @@ type ProofOfStake struct {
 }
 
 type Network struct {
-	Bind              string        `env:"BIND"    env-default:"0.0.0.0:9123"                    yaml:"bind"`
+	Bind              string        `env:"BIND"           env-default:"0.0.0.0:9123"                    yaml:"bind"`
 	BrokerURI         string        `env:"BROKER_URI"     env-default:"wss://shinobi.brokers.kenshi.io" yaml:"uri"`
 	ReadHeaderTimeout time.Duration `env:"BROKER_TIMEOUT" env-default:"3s"                              yaml:"read_header_timeout"`
 }
