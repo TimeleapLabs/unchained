6e5d91674a5f41a5f73fdf5ade4bdb6959e3813f
Pouya <pouya@kenshi.io>
2024-04-24 23:44:34 +0200

    💚 ci(docker): fix docker build

--- docker/build.sh
+++ docker/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker buildx build --build-arg="UBUNTU_VERSION=latest" --build-arg="UNCHAINED_VERSION=v0.11.0" -t ghcr.io/kenshitech/unchained:latest --no-cache .
+docker buildx build --build-arg="UBUNTU_VERSION=latest" --build-arg="UNCHAINED_VERSION=v0.11.0" -t ghcr.io/timeleaplabs/unchained:latest --no-cache .
