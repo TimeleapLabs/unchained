38ee040140e5b4abfda4fc5bb026ff602f2b8bc6
Pouya <pouya@kenshi.io>
2024-06-03 13:45:02 +0200

    ✅ test: fix mock contracts

--- internal/service/pos/pos_mock.go
+++ internal/service/pos/pos_mock.go
@@ -27,6 +27,11 @@ func (m *MockService) GetVotingPower(address [20]byte, block *big.Int) (*big.Int
 	return big.NewInt(int64(args.Int(0))), args.Error(1)
 }
 
+func (m *MockService) GetVotingPowerOfEvm(address [20]byte, block *big.Int) (*big.Int, error) {
+	args := m.Called(address, block)
+	return big.NewInt(int64(args.Int(0))), args.Error(1)
+}
+
 func (m *MockService) GetVotingPowerOfPublicKey(_ context.Context, pkBytes [96]byte) (*big.Int, error) {
 	args := m.Called(pkBytes)
 	return big.NewInt(int64(args.Int(0))), args.Error(1)
