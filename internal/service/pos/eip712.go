1755610108692b9823c357c0f478a99a98ba233d
Pouya <pouya@kenshi.io>
2024-06-03 13:45:02 +0200

    ✅ test: fix mock contracts

--- internal/service/pos/eip712.go
+++ internal/service/pos/eip712.go
@@ -7,6 +7,6 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-func (s *service) Slash(ctx context.Context, address [20]byte, to common.Address, amount *big.Int, nftIDs []*big.Int) error {
+func (s *service) Slash(_ context.Context, _ [20]byte, _ common.Address, _ *big.Int, _ []*big.Int) error {
 	return nil
 }
