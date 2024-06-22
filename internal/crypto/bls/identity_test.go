fadecbfb27259720e083b0de1ff6595075de5c82
Parya Rastegar <rastegar.parya3@gmail.com>
2024-06-22 04:51:27 +0330

    🐛 fix(lint): fix linters >>> ⏰ 5m

--- internal/crypto/bls/identity_test.go
+++ internal/crypto/bls/identity_test.go
@@ -1,9 +1,10 @@
 package bls
 
 import (
+	"testing"
+
 	"github.com/TimeleapLabs/unchained/internal/config"
 	"github.com/stretchr/testify/suite"
-	"testing"
 )
 
 const SamplePrivateKey = "3b885a8a8f043724abfa865eccd38f536887d9ea1c08a742720e810f38a86872"
