1f7f901ca5684b9a78123ef4864c9ca3dbab0f1f
Pouya <pouya@kenshi.io>
2025-02-04 11:56:15 +0100

    Bump version

--- internal/consts/meta.go
+++ internal/consts/meta.go
@@ -2,5 +2,5 @@ package consts
 
 // Meta information of the application
 
-var Version = "0.14.0-alpha"
-var ProtocolVersion = "0.14.0-alpha"
+var Version = "0.14.0-alpha.1"
+var ProtocolVersion = "0.14.0-alpha.1"
